2139e82bfd3a78b5ed95fdaee0d35efcb03fefdb
bombs0817 <paul.l@tc-gaming.com>
2020-09-17 18:51:34 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import videojs from 'video.js/dist/video'
+import 'video.js/dist/video-js.min.css'
 
 function App() {
+  const videoRef = useRef()
+  const player = useRef()
+
+  const setPlayerSource = () => {
+    const src = 'http://techslides.com/demos/sample-videos/small.mp4'
+    let type = 'application/x-mpegURL'
+    if (/(.mp4)/g.test(src)) type = 'video/mp4'
+    console.log(player.current.src)
+    player.current.src([{src, type}])
+  }
+
+  const onPlayerReday = () => {
+    console.log('player reday')
+  }
+
+  const onPlayerLoad = () => {
+    console.log(this)
+    console.log(player.current.videoWidth())
+    console.log(player.current.videoHeight())
+    // videoRef.current.width = player.current.videoWidth()
+    // videoRef.current.height = player.current.videoHeight()
+  }
+
+  useEffect(() => {
+    
+    const option = {
+      html5: {
+        nativeTextTracks: false // 隱藏mobile內建字幕按鈕
+      },
+      loop: true,
+      language: 'zh',
+      languages: {
+        zh: {
+          LIVE: ""
+        }
+      },
+      controlBar: {
+        pictureInPictureToggle: false
+      }
+    }
+    player.current = videojs(videoRef.current, option, onPlayerReday)
+    player.current.on('loadeddata', onPlayerLoad)
+    setPlayerSource()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div data-vjs-player>
+        <video
+          ref={videoRef}
+          id="my_video_1"
+          className="video-js vjs-default-skin"
+          controls
+          poster=""
+          data-setup="{}"
+        />
+      </div>
     </div>
   );
 }
